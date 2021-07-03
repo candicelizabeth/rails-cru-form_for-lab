7b01d0ebb97b432153177243674a209326dd50da
Candice Peters <candice.peters@flatironschool.com>
2021-07-03 11:44:03 -0500

    commit tree ac092537ff13679eb4a7a2a87202c91c5380ef40

---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,37 @@
+class ArtistsController < ApplicationController
+    def show 
+        @artist = Artist.find_by_id(params[:id])
+    end
+
+    def new 
+        @artist = Artist.new   
+    end
+
+    def create 
+        @artist = Artist.new(artist_params)
+        if @artist.save
+            redirect_to artist_path(@artist)
+        else
+            render :new
+        end
+    end
+
+    def edit 
+        @artist = Artist.find_by_id(params[:id])
+    end
+
+    def update 
+        @artist = Artist.find_by_id(params[:id])
+        if @artist.update(artist_params)
+            redirect_to artist_path(@artist)
+        else
+            render :edit
+        end
+    end
+
+    private 
+
+    def artist_params
+        params.require(:artist).permit(:name, :bio)
+    end
+end
